b072b1009a4e8d1b279f67fc168d2f7a80dff948
Tatip003 <ts.penafielm@uea.edu.ec>
2025-12-10 18:54:42 -0500

    tarea
    tarea

--- Semana03/Registroestudiante.cs
+++ Semana03/Registroestudiante.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RegistroEstudiantes
+{
+    // Clase que representa a un Estudiante
+    public class Estudiante
+    {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+
+        // Array para almacenar 3 números de teléfono
+        public string[] Telefonos { get; set; }
+
+        public Estudiante()
+        {
+            Telefonos = new string[3];
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("\n=== DATOS DEL ESTUDIANTE ===");
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"Nombres: {Nombres}");
+            Console.WriteLine($"Apellidos: {Apellidos}");
+            Console.WriteLine($"Dirección: {Direccion}");
+            Console.WriteLine("Teléfonos:");
+            for (int i = 0; i < Telefonos.Length; i++)
+            {
+                Console.WriteLine($"  Teléfono {i + 1}: {Telefonos[i]}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante = new Estudiante();
+
+            Console.WriteLine("=== REGISTRO DE ESTUDIANTE ===");
+
+            Console.Write("Ingrese ID: ");
+            estudiante.ID = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese nombres: ");
+            estudiante.Nombres = Console.ReadLine();
+
+            Console.Write("Ingrese apellidos: ");
+            estudiante.Apellidos = Console.ReadLine();
+
+            Console.Write("Ingrese dirección: ");
+            estudiante.Direccion = Console.ReadLine();
+
+            Console.WriteLine("\n--- Registro de teléfonos ---");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Ingrese teléfono {i + 1}: ");
+                estudiante.Telefonos[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nRegistro completado.");
+
+            // Mostrar la información ingresada
+            estudiante.MostrarDatos();
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
+
